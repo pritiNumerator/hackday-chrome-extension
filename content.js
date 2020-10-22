32a491b4264e84cfce325627ee2caf1b4d6d9f6e
pritiNumerator <priti.kumari@numerator.com>
2020-10-22 18:11:30 +0530

    changes for content.js

--- content.js
+++ content.js
@@ -3,12 +3,18 @@ function gotMessage(message,sender,sendresponse)
 {
 	console.log(message.txt);
 	let paragraphs = document.getElementsByTagName("div");
+	// document.activeElement.style['background-color'] = '#000000';
+	document.activeElement.style['color'] = '#ffffff';
+	document.head.style['background-color'] = '#000000';
+	document.head.style['color'] = '#ffffff';
 	for(elt of paragraphs)
 	{
-		elt.style['background-color'] = '#00CED1';
+		elt.style['background-color'] = '#000000';
+		elt.style['color'] = '#ffffff';
 	}
+	if(message.tt) {
+		console.log('tab found '+tt.tf);
+	}
+	
+
 }
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) { return {cancel: true}; },
-    {urls: ["https://en.wikipedia.org/wiki/Adele"]},
-    ["blocking"]);
